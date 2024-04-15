2d23d7a996281c4af86fe1d8ca1c741676a2f408
raphaeleliass <raphaeleliass@outlook.com>
2024-04-15 19:57:14 -0300

    feat(website): add fylo landingpage

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# fylo-landing-page
- 
+# Fylo Landingpage Project
+
+This project contains the Fylo Landingpage project.
+
+## Overview
+
+### Screenshot
+
+![Mobile Preview](src/design/mobile-preview.png)
+![Desktop Preview](src/design/desktop-preview.png)
+
+### Links
+
+- Live Site URL: [Fylo Landingpage](https://raphaelelias-fylo-landingpage.vercel.app)
+
+### Built With
+
+- HTML
+- Tailwind CSS
+
+### Useful Resources
+
+- [Tailwind CSS Documentation](https://tailwindcss.com/docs) - Helpful for understanding Tailwind CSS classes and features.
+
+## Author
+
+- Linkedin - [Linkedin](https://www.linkedin.com/in/raphaeleliass)
+- GitHub - [Github](https://www.github.com/raphaeleliass)
+- My Projects - [Portfolio](https://raphaelelias.vercel.app)
+
+## Acknowledgments
+
+- Coded by [Raphael Elias](https://www.github.com/raphaeleliass).
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDarkBlue: "hsl(217, 28%, 15%)",
+        primaryDarkBlue_main: "hsl(218, 28%, 13%)",
+        primaryDarkBlue_footer: "hsl(216, 53%, 9%)",
+        primaryDarkBlue_testimonials: "hsl(219, 30%, 18%)",
+        accentCyan: "hsl(176, 68%, 64%)",
+        accentBlue: "hsl(198, 60%, 50%)",
+        accentLightRed: "hsl(0, 100%, 63%)",
+        neutralWhite: "hsl(0, 0%, 100%)",
+      },
+      fontSize: {
+        bodycopy: "14px",
+      },
+      fontFamily: {
+        Raleway: ["Raleway", "Arial", "Sans-Serif"],
+        OpenSans: ["Open Sans", "Arial", "Sans-Serif"],
+      },
+      fontWeight: {
+        Raleway400: "400",
+        Raleway700: "700",
+        OpenSans400: "400",
+        OpenSans700: "700",
+      },
+      backgroundImage: {
+        desktopCurvy: "url('/src/images/bg-curvy-desktop.svg')",
+        mobileCurvy: "url('/src/images/bg-curvy-mobile.svg')",
+      },
+    },
   },
   plugins: [],
 };
